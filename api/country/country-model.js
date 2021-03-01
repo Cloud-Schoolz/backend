de8f891337f73d46dc45af124f702db0d69b2ff1
CerritoCode0101 <CaseyCerrito@gmail.com>
2021-02-28 23:16:25 -0600

    yay!

--- api/country/country-model.js
+++ api/country/country-model.js
@@ -1,20 +1,27 @@
-const db = require("../../data/db-config")
+const { where } = require("../../data/db-config");
+const db = require("../../data/db-config");
 
 module.exports = {
-    
-    find,
-    findBy,
-    findById,
+  find,
+  findBy,
+  findById,
+  volCountry
+};
+
+function volCountry(id) {
+  return db("country as c")
+    .join("volunteers as v", "c.id", "v.volunteer_id")
+    .select("c.name", "v.name").where("v.id",id)
 }
 
 function find() {
-    return db("country").select("name").orderBy("id")
+  return db("country").select("name").orderBy("id");
 }
 
 function findBy(filter) {
-    return db("country").where(filter).orderBy("id")
+  return db("country").where(filter).orderBy("id");
 }
 
 function findById(id) {
-    return db("country").select("name").where({ id: id }).first()
+  return db("country").select("name").where({ id: id }).first();
 }
--- api/country/country-router.js
+++ api/country/country-router.js
@@ -3,21 +3,42 @@ const express = require("express")
 const router = express.Router();
 
 
+
+
+// router.get("/volcountry", (req, res) => {
+//     Country.volCountry().then(( country ) => {
+//         res.status(200).json( country );
+//     }).catch((err) => res.status(500).json({ message: err.messege }));
+// });
+
+
+router.get("/volcountry/:id",(req, res) => {
+    const idVar = req.params.id
+    Country.volCountry(idVar).then((country) => {
+        if (!country) {
+            res.status(404).json({ message: "The country with the specified ID does not exist" });
+        }else {
+            res.status(200).json(country);
+        }
+    }).catch(() => res.status(500).json({ message: "The country information could not be retrieved" }));
+});
+
+
 router.get("/", (req, res) => {
     Country.find().then(( country ) => {
         res.status(200).json( country );
-    }).catch(() => res.status(500).json({ message: "The task information could not be retrieved" }));
+    }).catch(() => res.status(500).json({ message: "The country information could not be retrieved" }));
 });
 
 router.get("/:id",(req, res) => {
     const idVar = req.params.id
     Country.findById(idVar).then((country) => {
         if (!country) {
-            res.status(404).json({ message: "The task with the specified ID does not exist" });
+            res.status(404).json({ message: "The country with the specified ID does not exist" });
         }else {
             res.status(200).json(country);
         }
-    }).catch(() => res.status(500).json({ message: "The task information could not be retrieved" }));
+    }).catch(() => res.status(500).json({ message: "The country information could not be retrieved" }));
 });
 
 module.exports = router;
