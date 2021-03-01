65a06feb6ead01537cc0c64dd120b70c362d1bda
CerritoCode0101 <CaseyCerrito@gmail.com>
2021-02-28 21:24:06 -0600

    ksdjfghsdg

--- data/seeds/03_admin.js
+++ data/seeds/03_admin.js
@@ -2,7 +2,8 @@
 exports.seed = function (knex) {
   // 000-cleanup.js already cleaned out all tables
 
-  const admin = [{
+  const admin = [
+    {
     name: "admin0",
     email: "admin0@student.com",
     password: "password"
